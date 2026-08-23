eabf69fe479c12bbfdace4799040a3d8a339399b
prince9118 <pchaudharyskn@gmail.com>
2026-08-23 22:13:07 +0530

    city services

--- src/modules/cities/city.service.ts
+++ src/modules/cities/city.service.ts
@@ -1,8 +1,44 @@
 import { prisma } from "../../config/prisma.js";
-import type {
-    CreateCityInput,UpdateCityInput
-} from "./city.schema.js";
+import type { CreateCityInput, UpdateCityInput } from "./city.schema.js";
 
-export async function createCity(input:CreateCityInput){
-    return prisma.city.create
+export async function createCity(input: CreateCityInput) {
+  return prisma.city.create({
+    data: {
+      name: input.name,
+      state: input.state
+    }
+  });
+}
+
+export async function getCities() {
+  return prisma.city.findMany({
+    orderBy: {
+      name: "asc"
+    }
+  });
+}
+
+export async function getCityById(cityId: string) {
+  return prisma.city.findUnique({
+    where: {
+      id: cityId
+    }
+  });
+}
+
+export async function updatCity(cityId: string, input: UpdateCityInput) {
+  return prisma.city.update({
+    where: {
+      id: cityId
+    },
+    data: input
+  });
+}
+
+export async function deleteCity(cityId: string) {
+  return prisma.city.delete({
+    where: {
+      id: cityId
+    }
+  });
 }
