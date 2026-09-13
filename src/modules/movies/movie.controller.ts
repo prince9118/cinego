d768e7bbed6f35d8986689fc4fa9a987fdd406de
prince9118 <pchaudharyskn@gmail.com>
2026-09-13 23:02:19 +0530

    update in movieGet Controller

--- src/modules/movies/movie.controller.ts
+++ src/modules/movies/movie.controller.ts
@@ -54,51 +54,51 @@ export async function getMoviesController(_req: Request, res: Response) {
   }
 }
 
-export async function getMovieByIdController(req: Request, res: Response) {
-  try {
-    const movie = await getMovieById(req.params.id as string);
-    if (!movie) {
-      return res.status(404).json({
-        success: false,
-        message: "Movie not found"
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      data: movie
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error"
-    });
-  }
-}
+// export async function getMovieByIdController(req: Request, res: Response) {
+//   try {
+//     const movie = await getMovieById(req.params.id as string);
+//     if (!movie) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Movie not found"
+//       });
+//     }
+//     return res.status(200).json({
+//       success: true,
+//       data: movie
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({
+//       success: false,
+//       message: "Internal Server Error"
+//     });
+//   }
+// }
 
-export async function updateMovieController(req: Request, res: Response) {
-  const result = updateMovieSchema.safeParse(req.body);
-  if (!result.success) {
-    return res.status(400).json({
-      success: false,
-      message: "Validation failes",
-      errors: result.error.flatten()
-    });
-  }
-  try {
-    const movie = await updateMovie(req.params.id as string, result.data);
-    return res.status(200).json({
-      success: true,
-      data: movie
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error"
-    });
-  }
-}
+// export async function updateMovieController(req: Request, res: Response) {
+//   const result = updateMovieSchema.safeParse(req.body);
+//   if (!result.success) {
+//     return res.status(400).json({
+//       success: false,
+//       message: "Validation failes",
+//       errors: result.error.flatten()
+//     });
+//   }
+//   try {
+//     const movie = await updateMovie(req.params.id as string, result.data);
+//     return res.status(200).json({
+//       success: true,
+//       data: movie
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({
+//       success: false,
+//       message: "Internal Server Error"
+//     });
+//   }
+// }
 
 // export async function deleteMovieController(req: Request, res: Response) {
 //   try {
